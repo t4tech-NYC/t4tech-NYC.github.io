10069a71f5555aeadbd1b901c076f5ce89970a5a
Daly Barnett <dalyjbarnett@gmail.com>
2018-10-06 15:17:04 -0400

    nav bar functioning

--- app.js
+++ app.js
@@ -1,3 +1,33 @@
 $(document).ready(function() {
-  $()
+  $('section').hide()
+
+  $('.aboutButton').click(function () {
+    $('section').hide()
+    $('.aboutSection').toggle()
+  })
+
+  $('.cocButton').click(function () {
+    $('section').hide()
+    $('.codeOfConductSection').toggle()
+  })
+
+  $('.upcomingEventsButton').click(function () {
+    $('section').hide()
+    $('.upcomingEventsSection').toggle()
+  })
+
+  $('.previousEventsButton').click(function () {
+    $('section').hide()
+    $('.previousEventsSection').toggle()
+  })
+
+  $('.collaboratorsButton').click(function () {
+    $('section').hide()
+    $('.collaboratorsSection').toggle()
+  })
+
+  $('.contactButton').click(function () {
+    $('section').hide()
+    $('.contactSection').toggle()
+  })
 })
