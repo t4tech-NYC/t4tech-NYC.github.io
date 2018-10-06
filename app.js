825594f65476aa2f8de327b64d84ee181ed71bfd
Daly Barnett <dalyjbarnett@gmail.com>
2018-10-06 15:33:09 -0400

    opens at about page

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 $(document).ready(function() {
   $('section').hide()
 
+  $('.aboutSection').toggle()
+
   $('.aboutButton').click(function () {
     $('section').hide()
     $('.aboutSection').toggle()
